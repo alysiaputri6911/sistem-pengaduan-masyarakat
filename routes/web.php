3928e86de9bea6fb0d990535ae16e1048db924ad
alysiaputri6911 <alysiaputri6911@gmail.com>
2026-06-29 23:11:07 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComplaintController;
 
-Route::get('/', function () {
-    return redirect('/dashboard');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard', [
+        'total' => 0,
+        'open' => 0,
+        'progress' => 0,
+        'resolved' => 0,
+    ]);
+})->middleware(['auth'])->name('dashboard');
 
 Route::get('/dashboard', function () {
 
