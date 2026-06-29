ecebdf34502b47874b426bac1f848394ec722929
alysiaputri6911 <alysiaputri6911@gmail.com>
2026-06-29 23:56:24 +0700

    dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,18 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\ComplaintController as AdminComplaintController;
 use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\DashboardController;
+
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
 
 Route::middleware(['auth'])->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
 
-    // Citizen
     Route::resource('complaints', ComplaintController::class);
-
-    // Admin
-    Route::prefix('admin')
-        ->name('admin.')
-        ->group(function () {
-
-            Route::resource(
-                'complaints',
-                AdminComplaintController::class
-            )->only([
-                'index',
-                'show',
-                'edit',
-                'update'
-            ]);
-
-        });
-
 });
+
+require __DIR__.'/auth.php';
